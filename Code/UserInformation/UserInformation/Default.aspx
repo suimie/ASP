e313cff638557e4fd0a8d83981c6a61c21b09c42
DESKTOP-6RB3LQV\suimi <suimieya@gmail.com>
2018-06-05 21:43:11 -0400

    *User Information - Exercise

---
+++ Code/UserInformation/UserInformation/Default.aspx
@@ -0,0 +1,76 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" Async="true" CodeBehind="Default.aspx.cs" Inherits="UserInformation.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <style type="text/css">
+        .auto-style1 {
+            font-weight: bold;
+            font-size: xx-large;
+        }
+        .auto-style2 {
+            width: 100%;
+        }
+        .auto-style3 {
+            width: 169px;
+        }
+        .auto-style4 {
+            text-decoration: underline;
+            font-size: larger;
+        }
+    </style>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+        </div>
+        <span class="auto-style1">User Information</span><table class="auto-style2">
+            <tr>
+                <td class="auto-style3">Name</td>
+                <td>
+                    <asp:TextBox ID="txtName" runat="server"></asp:TextBox>
+                </td>
+            </tr>
+            <tr>
+                <td class="auto-style3">Date of Birth</td>
+                <td>
+                    <asp:TextBox ID="txtBirthDate" runat="server"></asp:TextBox>
+                </td>
+            </tr>
+            <tr>
+                <td class="auto-style3">Email</td>
+                <td>
+                    <asp:TextBox ID="txtEmail" runat="server"></asp:TextBox>
+                </td>
+            </tr>
+            <tr>
+                <td class="auto-style3">Provence</td>
+                <td>
+                    <asp:DropDownList ID="ddlProvence" runat="server" AutoPostBack="true" Height="20px" OnSelectedIndexChanged="ddlProvence_SelectedIndexChanged" Width="130px">
+                    </asp:DropDownList>
+                </td>
+            </tr>
+            <tr>
+                <td class="auto-style3">City</td>
+                <td>
+                    <asp:DropDownList ID="ddlCity" runat="server" Height="20px" Width="130px">
+                    </asp:DropDownList>
+                </td>
+            </tr>
+        </table>
+        <br />
+        <br />
+        <asp:Button ID="btnSubmit" runat="server" Text="Submit" OnClick="btnSubmit_Click" />
+&nbsp;&nbsp;&nbsp;
+        <br />
+        <br />
+        <asp:Label ID="lblInfo" runat="server"></asp:Label>
+        <br />
+        <br />
+        <span class="auto-style4"><strong>Submitted users</strong></span><br />
+        <asp:ListBox ID="lbUsers" runat="server" Width="417px"></asp:ListBox>
+    </form>
+</body>
+</html>
---
+++ Code/UserInformation/UserInformation/Default.aspx.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace UserInformation
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (ddlProvence.Items.Count <= 0)
+            {
+                ddlProvence.Items.Add("Ontario");
+                ddlProvence.Items.Add("Quebec");
+                ddlProvence.Items.Add("Nova Scotia");
+                ddlProvence.Items.Add("New Brunswick");
+                ddlProvence.Items.Add("Manitoba");
+                ddlProvence.Items.Add("British Columbia");
+                ddlProvence.Items.Add("Prince Edward Island");
+                ddlProvence.Items.Add("Saskatchewan");
+                ddlProvence.Items.Add("Alberta");
+                ddlProvence.Items.Add("Newfoundland and Labrador");
+            }
+        }
+
+        protected void ddlProvence_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            ddlCity.Items.Clear();
+            switch (ddlProvence.SelectedIndex)
+            {
+                case 0:
+                    ddlCity.Items.Add("Toronto");
+                    break;
+                case 1:
+                    ddlCity.Items.Add("Quebec city");
+                    ddlCity.Items.Add("Montreal");
+                    break;
+                case 2:
+                    ddlCity.Items.Add("Halifax");
+                    break;
+                case 3:
+                    ddlCity.Items.Add("Fredericton");
+                    ddlCity.Items.Add("Moncton");
+                    break;
+                case 4:
+                    ddlCity.Items.Add("Winnipeg");
+                    break;
+                case 5:
+                    ddlCity.Items.Add("Victoria");
+                    ddlCity.Items.Add("Vancouver");
+                    break;
+                case 6:
+                    ddlCity.Items.Add("Charottetown");
+                    break;
+                case 7:
+                    ddlCity.Items.Add("Regina");
+                    ddlCity.Items.Add("Saskatoon");
+                    break;
+                case 8:
+                    ddlCity.Items.Add("Edmonton");
+                    ddlCity.Items.Add("Calgary");
+                    break;
+                case 9:
+                    ddlCity.Items.Add("St. Jonh's");
+                    break;
+            }
+        }
+
+        protected void btnSubmit_Click(object sender, EventArgs e)
+        {
+            lblInfo.Text = txtName.Text + " : " + txtBirthDate.Text + ", " + txtEmail.Text + ", " + ddlProvence.SelectedValue + ", " + ddlCity.SelectedValue;
+            lbUsers.Items.Add(lblInfo.Text);
+            txtName.Text = "";
+            txtBirthDate.Text = "";
+            txtEmail.Text = "";
+            ddlProvence.SelectedIndex = 0;
+        }
+
+
+    }
+}
